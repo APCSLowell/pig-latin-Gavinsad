d061f59e76c0c194b75731619ab12e5aa5239a4e
Gavinsad <gali6@s.sfusd.edu>
2025-01-24 11:36:10 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -33,24 +33,14 @@ public int findFirstVowel(String sWord){
 for(int i=0;i<sWord.length();i++){
     if(sWord.substring(i,i+1).equals("a")||sWord.substring(i,i+1).equals("e")||sWord.substring(i,i+1).equals("i")||sWord.substring(i,i+1).equals("o")||sWord.substring(i,i+1).equals("u")){return i;}
   }
-  return -1;
+return -1;
 }
 
 
 public String pigLatin(String sWord)
 {
-  if(findFirstVowel(sWord) == -1)
-  {
-    return sWord + "ay";
-  }
-  if(sWord.substring(0,2).equals("qu")){
-   return sWord.substring(2) + "quay"; 
-  }
-  if(findFirstVowel(sWord)==0){
-    return sWord + "way";
-  }
-  else
-  {
-    return sWord.substring(findFirstVowel(sWord))+ sWord.substring(0,findFirstVowel(sWord)) + "ay";
-  }
+  if(findFirstVowel(sWord) == -1){  return sWord + "ay"; }
+  if(sWord.substring(0,2).equals("qu"))  {return sWord.substring(2) + "quay";  }
+  if(findFirstVowel(sWord)==0){ return sWord + "way";  }
+  else {return sWord.substring(findFirstVowel(sWord))+ sWord.substring(0,findFirstVowel(sWord)) + "ay";}
 }
